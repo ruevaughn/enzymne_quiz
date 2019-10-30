3d4b327d3fb85d099655bc3e8d4142af9e256c30
Chase Jensen <soccerchase@gmail.comm>
2019-10-30 03:31:29 -0600

    inital Commit

---
+++ CraqValidator.rb
@@ -0,0 +1,20 @@
+class CraqValidator
+
+  attr_reader :questions, :answers, :errors
+
+  def initialize(questions, answers)
+   
+    @questions  = questions
+    @answers    = answers
+    @errors     = {}
+  end
+
+  def valid?
+    # Implement your validation logic here, and return true
+    # if the result is valid and false if not.
+    
+    # Also set the errors hash correctly, for example:
+
+    # errors[:q0] = "was not answered"
+  end
+end
