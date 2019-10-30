363f44df18b6cecf3488ec8d8893b77dc09c069d
Chase Jensen <soccerchase@gmail.comm>
2019-10-30 04:15:02 -0600

    first assertion

--- craq_valiator.rb
+++ craq_valiator.rb
@@ -7,6 +7,7 @@ class CraqValidator
     @questions  = questions
     @answers    = answers
     @errors     = {}
+    @valid      = false
   end
 
   def valid?
@@ -16,5 +17,23 @@ class CraqValidator
     # Also set the errors hash correctly, for example:
 
     # errors[:q0] = "was not answered"
+
+    if answer_has_valid_format 
+      @questions.each do |question, options|
+      end
+    else
+      @valid = false
+      @errors[:q0] = "was not answered"
+    end
+
+
   end
+
+  def answer_has_valid_format
+    (@answers.nil? || answers == {}) ? false : true
+  end
+
+  question1 = [{ text: 'q1', options: [{ text: 'an option' }, { text: 'another option' }] }]
+  answers1  =  {}
+  CraqValidator.new(question1, answers1)
 end
