e3f6808e6faa22f5435f1297f1ba1f00a6390dee
Chase Jensen <soccerchase@gmail.comm>
2019-10-30 03:34:51 -0600

    Run Rubocop

--- craq_valiator.rb
+++ craq_valiator.rb
@@ -1,9 +1,9 @@
-class CraqValidator
+# frozen_string_literal: true
 
+class CraqValidator
   attr_reader :questions, :answers, :errors
 
   def initialize(questions, answers)
-   
     @questions  = questions
     @answers    = answers
     @errors     = {}
@@ -12,7 +12,7 @@ class CraqValidator
   def valid?
     # Implement your validation logic here, and return true
     # if the result is valid and false if not.
-    
+
     # Also set the errors hash correctly, for example:
 
     # errors[:q0] = "was not answered"
